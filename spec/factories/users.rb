60c74044288849472c7c615fe0431c13d54de0d8
norifumi0129 <lorentz.2806@gmail.com>
2023-11-20 16:24:35 +0900

    パスワードカラムの修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname  { Faker::Name.unique.name } #ニックネームのようなダミーデータの生成
     email      { Faker::Internet.unique.email } # メールアドレスのようなダミーデータの生成
-    password { Faker::Internet.password(min_length: 6) }
+    password { Faker::Alphanumeric.alphanumeric(number: 6) + Faker::Internet.password(min_length: 1, max_length: 1, mix_case: true, special_characters: true) }
     password_confirmation { password }
     family_name { Faker::Name.last_name }
     first_name { Faker::Name.first_name }
