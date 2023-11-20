3b25d6a7247136a4479d9f81e6acd4ebd5b7f5e4
norifumi0129 <lorentz.2806@gmail.com>
2023-11-20 16:19:04 +0900

    バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
     with_options presence: true do
       validates :nickname
-      validates :family_name
-      validates :first_name
-      validates :family_name_kana
-      validates :first_name_kana
       validates :birthday
     end
+    with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/u, message: "氏名は全角でお願いします。" } do
+      validates :family_name
+      validates :first_name
+    end
+    with_options presence: true, format: { with: /\A[ァ-ヴー]+\z/u, message: "カナは全角カタカナでお願いします。" } do
+      validates :family_name_kana
+      validates :first_name_kana
+    end
+    validates :password, format: { with:/\A(?=.*[a-z])(?=.*\d)[a-z\d]{6,}\z/i,message:"パスワードは英数字混合の６文字以上です。"}
 end
