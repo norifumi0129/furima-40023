b1c879253844cf52a2f332297d64242fed110741
norifumi0129 <lorentz.2806@gmail.com>
2023-11-20 14:31:17 +0900

    pull request前　調整

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname  { Faker::Name.nickname } #ニックネームのようなダミーデータの生成
-    email      { Faker::Internet.email } # メールアドレスのようなダミーデータの生成
-    password {Faker::Internet.password(min_length: 6)}
-    password_confirmation {password}
-    family_name { Faker::Name.family_name}
-    first_name { Faker::Name.first_name}
-    family_name_kana { Faker::Name.family_name_kana}
-    first_name_kana { Faker::Name.first_name_kana}
-    birthday { Faker::Date.birthday}
+    nickname  { Faker::Name.unique.name } #ニックネームのようなダミーデータの生成
+    email      { Faker::Internet.unique.email } # メールアドレスのようなダミーデータの生成
+    password { Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
+    family_name { Faker::Name.last_name }
+    first_name { Faker::Name.first_name }
+    family_name_kana { Faker::Name.name }
+    first_name_kana { Faker::Name.name }
+    birthday { Faker::Date.birthday(min_age: 18, max_age: 65) }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,9 +7,7 @@ RSpec.describe User, type: :model do
 
   describe 'ユーザー新規登録' do
     context '新規登録できる場合' do
-      it "nicknameとemail、passwordとpassword_confirmation、
-        family_nameとfirst_name、family_name_kanaとfirst_name_kana、
-        birthdayが存在すれば登録できる" do
+      it "nicknameとemail、passwordとpassword_confirmation、family_nameとfirst_name、family_name_kanaとfirst_name_kana、birthdayが存在すれば登録できる" do
         expect(@user).to be_valid
       end
     end
@@ -17,7 +15,7 @@ RSpec.describe User, type: :model do
       it "nicknameが空では登録できない" do
         @user.nickname = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include("Nick can't be blank")
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
       end
       it "emailが空では登録できない" do
         @user.email = ''
@@ -32,22 +30,22 @@ RSpec.describe User, type: :model do
       it "family_nameが空では登録できない" do
         @user.family_name = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include("Family_name can't be blank")
+        expect(@user.errors.full_messages).to include("Family name can't be blank")
       end
       it "first_nameが空では登録できない" do
         @user.first_name = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include("First_name_kana can't be blank")
+        expect(@user.errors.full_messages).to include("First name can't be blank")
       end
       it "family_name_kanaが空では登録できない" do
         @user.family_name_kana = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include("Family_name_kana can't be blank")
+        expect(@user.errors.full_messages).to include("Family name kana can't be blank")
       end
       it "first_name_kanaが空では登録できない" do
         @user.first_name_kana = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include("First_name_kana can't be blank")
+        expect(@user.errors.full_messages).to include("First name kana can't be blank")
       end
       it "birthdayが空では登録できない" do
         @user.birthday = ''
