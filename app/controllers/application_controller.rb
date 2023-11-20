dfcc0efd00f719cfc0654a894a787460b1699f6c
norifumi0129 <lorentz.2806@gmail.com>
2023-11-20 16:20:23 +0900

    application_controllerの編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birthday])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birthday])
   end
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
