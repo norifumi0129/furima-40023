9dee73f4235f31d21f9aefb8a4a267793dc4f7b5
norifumi0129 <lorentz.2806@gmail.com>
2023-11-21 17:06:10 +0900

    active storage

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,16 @@
 class ItemsController < ApplicationController
   def index
   end
+  def new
+    @item = Item.new
+  end
+  def create
+    Item.create(item_params)
+    redirect_to '/'
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :introduction, :price, :category_id, :condition_id, :postage_payer_id, :preparation_day_id, :prefecture_id, :user_id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,19 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+  
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :introduction
+    validates :price
+    validates :category_id
+    validates :condition_id
+    validates :postage_payer_id
+    validates :preparation_day_id
+    validates :prefecture_id
+  end
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
   belongs_to :category
