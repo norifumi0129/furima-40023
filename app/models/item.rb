b73faf3f9e9a13df7b42f43162100b67bfc57ac4
norifumi0129 <lorentz.2806@gmail.com>
2023-11-24 14:54:43 +0900

    修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,28 +2,22 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   
-  with_options presence: true do
-    validates :image
-    validates :name
-    validates :introduction
-    validates :price
+    validates :image, presence: true
+  
+  with_options numericality: { other_than: 1 } ,presence: true do
     validates :category_id
     validates :condition_id
     validates :postage_payer_id
     validates :preparation_day_id
     validates :prefecture_id
   end
-  with_options numericality: { other_than: 1 } do
-    validates :category_id
-    validates :condition_id
-    validates :postage_payer_id
-    validates :preparation_day_id
-    validates :prefecture_id
-  end
-  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
-  validates_format_of :price, with: /\A\d+\z/, message: "is not a number"
-  validates :name, format: { with: /\A.{1,40}\z/,message:"is too long (maximum is 40 characters)"}
-  validates :introduction, format: { with: /\A.{1,1000}\z/ ,message:"is too long (maximum is 1000 characters)"}
+  validates :price, presence: true
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  validates_format_of :price, with: /\A\d+\z/, message: "は半角数値のみ入力してください", if: -> { price.present? }
+
+  validates :name, presence: true, format: { with: /\A.{1,40}\z/,message:"is too long (maximum is 40 characters)"}
+  validates :introduction, presence: true, format: { with: /\A.{1,1000}\z/ ,message:"is too long (maximum is 1000 characters)"}
+  
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
   belongs_to :category
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,8 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Item, type: :model do
   before do
-    user = FactoryBot.create(:user)
-    @item = FactoryBot.build(:item, user: user)
+    @item = FactoryBot.build(:item)
   end
   describe '商品出品' do
     context '商品が出品できる' do
@@ -58,39 +57,34 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
       end
       it 'priceは半角数字以外で入力すると出品できない' do
-        @item = Item.new(price: 'abc')
+        @item.price = '１１1'
         @item.valid?
-        expect(@item.errors[:price]).to include("is not a number")
+        expect(@item.errors.full_messages).to include("Price is not a number")
       end
       it 'category_idが1なら出品できない' do
-        category_id = 1
-        item = FactoryBot.build(:item, user: @user, category_id: category_id)
-        expect(item).not_to be_valid
-        expect(item.errors[:category_id]).to include("must be other than 1")
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category must be other than 1")
       end
       it 'condition_idが1なら出品できない' do
-        condition_id = 1
-        item = FactoryBot.build(:item, user: @user, condition_id: condition_id)
-        expect(item).not_to be_valid
-        expect(item.errors[:condition_id]).to include("must be other than 1")
+        @item.condition_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition must be other than 1")
       end
       it 'postage_payer_idが1なら出品できない' do
-        postage_payer_id = 1
-        item = FactoryBot.build(:item, user: @user, postage_payer_id: postage_payer_id)
-        expect(item).not_to be_valid
-        expect(item.errors[:postage_payer_id]).to include("must be other than 1")
+        @item.postage_payer_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Postage payer must be other than 1")
       end
       it 'prefecture_idが1ならなら出品できない' do
-        prefecture_id = 1
-        item = FactoryBot.build(:item, user: @user, prefecture_id: prefecture_id)
-        expect(item).not_to be_valid
-        expect(item.errors[:prefecture_id]).to include("must be other than 1")
+        @item.prefecture_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
       end
       it 'preparation_day_idが1なら出品できない' do
-        preparation_day_id = 1
-        item = FactoryBot.build(:item, user: @user, preparation_day_id: preparation_day_id)
-        expect(item).not_to be_valid
-        expect(item.errors[:preparation_day_id]).to include("must be other than 1")
+        @item.preparation_day_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Preparation day must be other than 1")
       end
       it 'ユーザーが紐付いていなければ投稿できない' do
         @item.user = nil
