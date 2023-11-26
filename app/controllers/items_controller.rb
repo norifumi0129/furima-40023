025aa1f5862bcb980e81a4383b1d55d117db05d3
norifumi0129 <lorentz.2806@gmail.com>
2023-11-26 10:04:29 +0900

    編集画面への遷移・編集ビュー作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,6 +26,19 @@ class ItemsController < ApplicationController
     @preparation_day = PreparationDay.find_by_id @item.preparation_day_id
   end
 
+  def edit
+    @item = Item.find(params[:id])
+    unless current_user == @item.user
+      redirect_to root_path, alert: "You don't have permission to edit this item."
+    end
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    redirect_to item_path
+  end
+
   def self.search(search)
     if search != ''
       Item.where('text LIKE(?)', "%#{search}%")
@@ -35,7 +48,6 @@ class ItemsController < ApplicationController
   end
 
   private
-
   def item_params
     params.require(:item).permit(:name, :introduction, :category_id, :condition_id, :postage_payer_id, :preparation_day_id,
                                  :prefecture_id, :price, :image).merge(user_id: current_user.id)
