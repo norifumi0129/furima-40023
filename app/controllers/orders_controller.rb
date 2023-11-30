4039be914041249fb758b156a75da4d326d4e613
norifumi0129 <lorentz.2806@gmail.com>
2023-11-30 10:37:13 +0900

    Revert "購入"
    This reverts commit 376bdc5099ba88702fa07933cb93e37068506399.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -18,7 +18,10 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: item.id)
+    params.permit(:item_id).merge(user_id: current_user.id)
   end
 
+  def address_params
+    params.permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(order_id: @order.id)
+  end
 end
