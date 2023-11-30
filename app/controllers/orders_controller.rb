8f8c821eb5d37fbb746210b2d42c2061eae782b2
norifumi0129 <lorentz.2806@gmail.com>
2023-11-30 09:42:23 +0900

    g

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,18 +7,18 @@ class OrdersController < ApplicationController
     @order_address = OrderAddress.new
   end
   def create
-    @item = Item.find(params[:item_id])
-    @order = Order.create(order_params.merge(item_id: @item.id))
-    Address.create(address_params)
-    redirect_to root_path
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
   end
   private
 
   def order_params
-    params.permit(:item_id).merge(user_id: current_user.id)
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: item.id)
   end
 
-  def address_params
-    params.permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(order_id: @order.id)
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  resources :items do
+    resources :orders
+  end
   devise_for :users
   root to: 'items#index'
-  resources :items do
-    resources :orders, only: [:create, :index]
-  end
 end
