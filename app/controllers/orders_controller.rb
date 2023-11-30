998564df4761803037178db22bed0a60da4f72f8
norifumi0129 <lorentz.2806@gmail.com>
2023-11-30 18:44:39 +0900

    購入で保存できない

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,22 +2,26 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!, except: :index
   def index
     @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new
   end
 
   def new
+    @order_address = OrderAddress.new
   end
 
   def create
     @item = Item.find(params[:item_id])
-    @order = Order.create(order_params.merge(item_id: @item.id))
-    Address.create(address_params)
-    redirect_to root_path
+    binding.pry
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
   end
   private
   def order_params
-    params.permit(:item_id).merge(user_id: current_user.id)
-  end
-  def address_params
-    params.permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(order_id: @order.id)
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
