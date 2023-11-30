229a3f95e79cd2c5895c7c880cb3727e68807c48
norifumi0129 <lorentz.2806@gmail.com>
2023-11-30 11:29:15 +0900

    購入　クレジットなし

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,24 +3,20 @@ class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
   end
+
   def new
-    @order_address = OrderAddress.new
   end
+
   def create
-    @order_address = OrderAddress.new(order_params)
-    if @order_address.valid?
-      @order_address.save
-      redirect_to root_path
-    else
-      render :index, status: :unprocessable_entity
-    end
+    @item = Item.find(params[:item_id])
+    @order = Order.create(order_params.merge(item_id: @item.id))
+    Address.create(address_params)
+    redirect_to root_path
   end
   private
-
   def order_params
     params.permit(:item_id).merge(user_id: current_user.id)
   end
-
   def address_params
     params.permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(order_id: @order.id)
   end
