cfe98c44ae3a3cca660a317c8e606c8ae57f58eb
norifumi0129 <lorentz.2806@gmail.com>
2023-12-04 18:47:47 +0900

    java script

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,16 +1,7 @@
-// グローバル変数としてPayjpのインスタンスを保持
-let payjpInstance;
-
-// turbolinks:load イベントが発生したときに実行されるコード
-$(document).on("turbolinks:load", function(){
-  const publicKey = gon.public_key;
-
-  // 既存のPayjpインスタンスが存在しない場合にのみ新しいインスタンスを作成
-  if (!payjpInstance) {
-    payjpInstance = Payjp(publicKey); // PAY.JPテスト公開鍵
-  }
-
-  const elements = payjpInstance.elements();
+const pay = () => {
+  const publicKey = gon.public_key
+  const payjp = Payjp(publicKey) // PAY.JPテスト公開鍵
+  const elements = payjp.elements();
   const numberElement = elements.create('cardNumber');
   const expiryElement = elements.create('cardExpiry');
   const cvcElement = elements.create('cardCvc');
@@ -18,13 +9,10 @@ $(document).on("turbolinks:load", function(){
   numberElement.mount('#number-form');
   expiryElement.mount('#expiry-form');
   cvcElement.mount('#cvc-form');
-  
-  const form = document.getElementById('charge-form');
-  
+  const form = document.getElementById('charge-form')
   form.addEventListener("submit", (e) => {
-    payjpInstance.createToken(numberElement).then(function (response) {
+    payjp.createToken(numberElement).then(function (response) {
       if (response.error) {
-        // エラーがある場合の処理
       } else {
         const token = response.id;
         const renderDom = document.getElementById("charge-form");
@@ -36,7 +24,9 @@ $(document).on("turbolinks:load", function(){
       cvcElement.clear();
       document.getElementById("charge-form").submit();
     });
-
     e.preventDefault();
   });
-});
+};
+
+window.addEventListener("load", pay);
+window.addEventListener("turbo:render", pay);
