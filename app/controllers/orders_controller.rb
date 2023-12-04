32f2a5343e5d5cb00682896f3b2567b00b687287
norifumi0129 <lorentz.2806@gmail.com>
2023-12-04 14:56:23 +0900

    commit

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -34,7 +34,7 @@ class OrdersController < ApplicationController
   end
 
   def order_params
-    params.require(:order_address).permit(:image, :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(
       user_id: current_user.id, item_id: params[:item_id], token: params[:token]
     )
   end
