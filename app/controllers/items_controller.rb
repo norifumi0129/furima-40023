bd1652af74fc06764a2b8481f797a4a5a16aa314
norifumi0129 <lorentz.2806@gmail.com>
2023-12-05 14:22:41 +0900

    Summary(required)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -73,9 +73,8 @@ class ItemsController < ApplicationController
   end
 
   def editing_prohibited
-    unless current_user == @item.user && !@item.id.nil?
+    return if current_user == @item.user && !@item.id.nil?
 
     redirect_to root_path
-    end
   end
 end
